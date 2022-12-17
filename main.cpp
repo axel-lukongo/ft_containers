0cd037812935acd6a0a70d612c3b92080bcb91d3
axel <lukongo.axel03@gmail.com>
2022-12-18 00:05:16 +0100

    i finish my iterator and i have soon finish the capacity

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: alukongo <alukongo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 21:05:51 by alukongo          #+#    #+#             */
-/*   Updated: 2022/12/16 21:18:42 by alukongo         ###   ########.fr       */
+/*   Updated: 2022/12/18 00:00:14 by alukongo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,15 @@
 
 int main(){
 	ft::vector<int> test;
-	ft::vector<int>::iterator it;
+	// ft::vector<int>::iterator it;
 	// test.ft_push_back(3);
 	// test.ft_push_back(2);
 	// test.ft_push_back(1);
-	it = test.begin();
-	std::cout << *it <<std::endl;
+	// it = test.begin();
+	// std::cout << *it <<std::endl;
+	std::cout << "size: "<<test.size() << std::endl;
+	std::cout << "capacity: "<<test.capacity() << std::endl;
+	test.reserve(3);
+	std::cout << "size: "<<test.size() << std::endl;
+	std::cout << "capacity: "<<test.capacity() << std::endl;
 }
--- vector.hpp
+++ vector.hpp
@@ -6,7 +6,7 @@
 /*   By: alukongo <alukongo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/09 15:33:58 by alukongo          #+#    #+#             */
-/*   Updated: 2022/12/16 21:36:20 by alukongo         ###   ########.fr       */
+/*   Updated: 2022/12/18 00:04:35 by alukongo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -152,17 +152,59 @@ public:
 	int size(){ return (_end - _start); }
 	int size_maxe(){return (allocator_type().max_size());}
 	
-	// void resize (size_type n, value_type val = value_type()){
-	// 	if (n == _size)
-	// 		return
-	// 	if (n < _size){
-			
+	// void fill_memory(pointer end, value_type val, size_type n){
+	// 	for (size_type i = 0; i < n; i++){
+	// 		_alloc.construct(end, val);
+	// 		end++;
 	// 	}
 	// }
-	int capacity(){ return _vec_capacity - _start;}
+	// void resize (size_type n, value_type val = value_type()){
+	// 	if (n < size()){
+	// 		for(pointer ptr = _start + n ;ptr < _end; ptr++)
+	// 			_alloc.destroy(ptr);
+	// 	}
+	// 	if (n > size())
+	// 	{
+	// 		if (n < _vec_capacity)
+	// 			//i will reserve for n
+	// 		if (n != size())
+	// 			fill_memory(_end, n - size(), val);
+	// 		_end = _start + n;
+	// 	}
+	// }
+
+	size_type capacity(){ return _vec_capacity - _start;}
 	
 	bool empty(){return ( (_end - _start) == 0);}
-	// void reserve(int){}
+	
+
+/******************************** reserve *********************************/
+	
+	void reserve (size_type n){
+		if (n > capacity()){
+			pointer new_vec;
+			new_vec = _alloc.allocate(n);
+			pointer ptr1 = new_vec;
+			pointer ptr2 = _start;
+			if (size())
+				while (ptr1 != _end)
+				{
+					_alloc.construct(ptr1, *ptr2);
+					ptr1++;
+					ptr2++;
+				}
+			for (pointer tmp = _start ;tmp != _end; tmp++)
+				_alloc.destroy(tmp);
+			_alloc.deallocate(_start, capacity());
+			_start = new_vec;
+			_end = ptr1;
+			_vec_capacity = _start + n;
+		}
+		else
+			_end = _start + n;
+	}
+
+
 	// void shrink_to_fit(){}
 
 
@@ -181,13 +223,13 @@ public:
 	// 	delete [] tmp;
 	// }
 
-	// void ft_push_back(value_type element){
-	// 	if (_capacity == _size){
-	// 		new_allocation();
-	// 	}
-	// 	_arr[_size] = element;
-	// 	_size++;
-	// }
+	void ft_push_back(value_type element){
+		if (_vec_capacity == size()){
+			// i reserve for 1
+		}
+		_alloc.construct(_end, element);
+		_end++;
+	}
 
 	// void ft_pop_back(){
 	// 	if (size() <= 0){
