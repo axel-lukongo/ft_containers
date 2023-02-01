1da35978b561a6b651ff60f6b85231ab4c5349bb
Axel LUKONGO <alukongo@e2r5p1.clusters.42paris.fr>
2023-02-01 17:31:49 +0100

    map is finish

--- map.hpp
+++ map.hpp
@@ -14,7 +14,7 @@ namespace ft{
 	template<	class Key, \
 				class T, \
 				class Compare = std::less<Key>, \
-				class Alloc = std::allocator<std::pair<const Key, T> > >
+				class Alloc = std::allocator<ft::pair<const Key, T> > >
 	class map{
 
 		public:
